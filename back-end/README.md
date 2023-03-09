cc603a79bf16d27b45080b42ed06a877d2c46eb7
ryantjakrakartadinata <rgtjakrakartadinata@dons.usfca.edu>
2023-03-09 01:17:10 -0800

    install express, initiate a server and add a readme

---
+++ back-end/README.md
@@ -0,0 +1,15 @@
+## Project Setup
+
+```sh
+npm init
+```
+
+## Dependencies
+```sh
+npm install express
+```
+
+### Run Server
+```sh
+node src/server.js
+```
---
+++ back-end/src/server.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Success!");
+})
+
+// Create a server that listens to the specified port
+app.listen(8000, () => console.log("Server is listening on port 8000"));
