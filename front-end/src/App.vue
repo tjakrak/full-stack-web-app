e8b89e95ac02617e1bb5c1fc873899b50fe35be8
ryantjakrakartadinata <rgtjakrakartadinata@dons.usfca.edu>
2023-03-10 12:57:52 -0800

    install dot, env and cors dependencies

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,107 +1,22 @@
-<script setup lang="ts">
+<script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import '@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.min.css'
-
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
-    <!-- <div class="slds-form-element">
-      <label class="slds-form-element__label" for="username">Username</label>
-      <div class="slds-form-element__control">
-        <input type="text" id="username" class="slds-input" />
-      </div>
-    </div>
-
-    <div>
-      <svg class="slds-icon slds-icon-text-default slds-icon_x-small">
-        <use xlink:href="@salesforce-ux/design-system/assets/icons/utility-sprite/svg/symbols.svg#search"></use>
-      </svg>
-    </div> -->
-
-    <!-- <div class="slds-accordion">
-      <template v-for="section in sections">
-        <section :class="{ 'slds-is-open': section.id === activeSectionId }" :key="section.id">
-          <div class="slds-accordion__summary" @click="activeSectionId = (activeSectionId === section.id ? null : section.id)">
-            <span class="slds-accordion__summary-action slds-text-link_reset">{{ section.title }}</span>
-          </div>
-          <div class="slds-accordion__content" v-show="section.id === activeSectionId">
-            {{ section.content }}
-          </div>
-        </section>
-      </template>
-    </div> -->
-
-    <ul class="slds-accordion">
-      <li class="slds-accordion__list-item">
-        <section class="slds-accordion__section slds-is-open">
-          <div class="slds-accordion__summary">
-            <h2 class="slds-accordion__summary-heading">
-              <button class="slds-button slds-button_reset slds-accordion__summary-action" aria-controls="referenceId-45" aria-expanded="true" title="Accordion summary">
-                <svg class="slds-accordion__summary-action-icon slds-button__icon slds-button__icon_left" aria-hidden="true">
-                  <use xlink:href="@salesforce-ux/design-system/assets/icons/utility-sprite/svg/symbols.svg#switch"></use>
-                </svg>
-                <span class="slds-accordion__summary-content">Accordion summary</span>
-              </button>
-            </h2>
-          </div>
-          <div hidden="" class="slds-accordion__content" id="referenceId-45">Accordion details - A</div>
-        </section>
-      </li>
-      <li class="slds-accordion__list-item">
-        <section class="slds-accordion__section">
-          <div class="slds-accordion__summary">
-            <h2 class="slds-accordion__summary-heading">
-              <button class="slds-button slds-button_reset slds-accordion__summary-action" aria-controls="referenceId-46" aria-expanded="false" title="Accordion summary">
-                <svg class="slds-accordion__summary-action-icon slds-button__icon slds-button__icon_left" aria-hidden="true">
-                  <use xlink:href="@salesforce-ux/design-system/assets/icons/utility-sprite/svg/symbols.svg#switch"></use>
-                </svg>
-                <span class="slds-accordion__summary-content">Accordion summary</span>
-              </button>
-            </h2>
-          </div>
-          <div hidden="" class="slds-accordion__content" id="referenceId-46">Accordion details - B</div>
-        </section>
-      </li>
-      <li class="slds-accordion__list-item">
-        <section class="slds-accordion__section">
-          <div class="slds-accordion__summary">
-            <h2 class="slds-accordion__summary-heading">
-              <button class="slds-button slds-button_reset slds-accordion__summary-action" aria-controls="referenceId-47" aria-expanded="false" title="Accordion summary">
-                <svg class="slds-accordion__summary-action-icon slds-button__icon slds-button__icon_left" aria-hidden="true">
-                  <use xlink:href="@salesforce-ux/design-system/assets/icons/utility-sprite/svg/symbols.svg#switch"></use>
-                </svg>
-                <span class="slds-accordion__summary-content">Accordion summary</span>
-              </button>
-            </h2>
-          </div>
-          <div hidden="" class="slds-accordion__content" id="referenceId-47">Accordion details - C</div>
-        </section>
-      </li>
-    </ul>
-    
-    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
-
-    <!-- <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
+    <div class="wrapper">
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <!-- <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink> -->
       </nav>
-    </div>  -->
+    </div>
   </header>
 
-  <!-- <RouterView /> -->
+  <RouterView />
 </template>
 
-<style lang="scss">
-  $static-font-path: '/assets/fonts/webfonts';
-  @import '@salesforce-ux/design-system/scss/index.scss';
-</style>
-
 <style scoped>
-/* 
 header {
   line-height: 1.5;
   max-height: 100vh;
@@ -162,6 +77,5 @@ nav a:first-of-type {
     padding: 1rem 0;
     margin-top: 1rem;
   }
-} */
-
+}
 </style>
---
+++ front-end/src/router/index.js
@@ -0,0 +1,57 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/LoginView.vue')
+    },
+    {
+      path: '/register',
+      name: 'register',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/RegisterView.vue')
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    }
+  ]
+})
+
+router.beforeEach(async(to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    // Authentication check
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      return next();
+    }
+
+    return next('/login');
+  }
+
+  next()
+})
+
+export default router
