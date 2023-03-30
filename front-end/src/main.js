43cb6410d5d79d4183ad8fe8b5e1c5d40fb3543c
ryantjakrakartadinata <rgtjakrakartadinata@dons.usfca.edu>
2023-03-29 18:02:37 -0700

    adding dotenv package library

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,10 +1,26 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
 import './assets/main.css'
+import dotenv from 'dotenv'
+import Cookies from 'js-cookie'
+
+// Load environment variables from .env file
+dotenv.config();
+
+const cookieMixin = {
+  created() {
+    Cookies.set('my_cookie', 'my_value', {
+      sameSite: 'none',
+      secure: true
+    })
+    this.cookieValue = Cookies.get('my_cookie')
+  }
+}
 
 const app = createApp(App)
+
+app.mixin(cookieMixin)
 
 app.use(router)
 
