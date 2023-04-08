623161e7268973229515ab36b3897215fbd53e1b
ryantjakrakartadinata <rgtjakrakartadinata@dons.usfca.edu>
2023-04-08 00:20:57 -0700

    add assessment view

---
+++ front-end/src/views/AssessmentView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Header from '../components/Header.vue'
+import Assessment from '../components/Assessment.vue'
+import Accordion from '../components/Accordion.vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute()
+console.log(route.params)
+</script>
+
+<template>
+  <main>
+    <Header></Header>
+    <!-- <Accordion></Accordion> -->
+    <Suspense>
+        <Assessment></Assessment>
+    </Suspense>
+  </main>
+</template>
